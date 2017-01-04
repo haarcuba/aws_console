4144f2ef8c43af289f5a5de49ca23393543652f7
Yoav Kleinberger <haarcuba@gmail.com>
2017-01-04 13:40:35 +0200

    first version

---
+++ boto_console.py
@@ -0,0 +1,72 @@
+#!/usr/bin/env python3
+import IPython
+import sys
+import time
+import os
+import boto3
+import subprocess
+import logging
+import re
+import argparse
+
+def say( message ):
+    sys.stderr.write( '{0}\n'.format( message ) )
+
+REGIONS = [ 'us-east-1', 'us-east-2', 'us-west-1', 'us-west-2', 'ca-central-1', 'eu-west-1', 'eu-central-1', 'eu-west-2', 'ap-northeast-1', 'ap-northeast-2', 'ap-southeast-1', 'ap-southeast-2', 'ap-south-1', 'sa-east-1', ]
+parser = argparse.ArgumentParser()
+parser.add_argument( 'region', choices = REGIONS )
+parser.add_argument( 'profile' )
+arguments = parser.parse_args()
+
+logging.basicConfig( level = logging.WARNING, format = '%(asctime)s %(levelname)s: %(message)s' )
+session = boto3.Session(profile_name=arguments.profile, region_name=arguments.region)
+ec2 = session.resource('ec2')
+
+def by_tag_values(pattern):
+    pattern = re.compile(pattern)
+    result = []
+    for machine in ec2.instances.all():
+        if not machine.tags:
+            continue
+        for tag in machine.tags:
+            match = pattern.search(tag['Value'])
+            if match is not None:
+                result.append(machine)
+
+    return result
+
+
+def ssh(user, instance, keyfile=None):
+    BASIC = ['ssh', '-l', user, '-X']
+    if keyfile:
+        KEY = ['-i', keyfile]
+    else:
+        KEY = []
+    instance.reload()
+    p = subprocess.Popen(BASIC + KEY + [instance.public_dns_name])
+    p.wait()
+
+def pollState( thing ):
+    state = thing.state
+    say( 'current state is {0}'.format( state ) )
+    thing.reload()
+    while thing.state == state:
+        time.sleep(1)
+        thing.reload()
+        print(".", end='', flush=True)
+        
+    say( 'current state is {0}'.format( state ) )
+    return thing.state
+
+def start( instance ):
+    instance.start()
+    instance.wait_until_running()
+
+def stop( instance ):
+    instance.stop()
+    instance.wait_until_stopped()
+
+def ping(instance):
+    os.system('ping {0}'.format(instance.public_dns_name))
+
+IPython.embed()

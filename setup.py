3ef30fd964a56d77a943efd03d2723c26fd51e22
Yoav Kleinberger <haarcuba@gmail.com>
2017-03-29 01:17:55 +0300

    version 0.1.4

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ README = 'a console for easily using AWS with boto3'
 requires = [ 'ipython >= 5.2.2', 'boto3', 'myconsole' ]
 
 setup(name='aws-console',
-      version='0.1.3',
+      version='0.1.4',
       description=README,
       long_description=README,
       url='https://github.com/haarcuba/aws_console',
